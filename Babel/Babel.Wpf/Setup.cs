26e4e1be00d61ce6613a61f6e8ed0fec874fcd60
Stuart <lodge.stuart@gmail.com>
2013-06-26 09:02:05 +0100

    Babel - Wpf - nuget package workarounds

--- Babel/Babel.Wpf/Setup.cs
+++ Babel/Babel.Wpf/Setup.cs
@@ -11,16 +11,6 @@ using Cirrious.MvvmCross.Wpf.Views;
 
 namespace Babel.Wpf
 {
-    public class HackMvxWpfResourceLoader : MvxWpfResourceLoader
-    {
-        public override void GetResourceStream(string resourcePath, Action<Stream> streamAction)
-        {
-            // in 3.0.8.2 and earlier we needed to replace the "/" with "\\" :/
-            resourcePath = resourcePath.Replace("/", "\\");
-            base.GetResourceStream(resourcePath, streamAction);
-        }
-    }
-
     // hack necessary due to https://github.com/slodge/MvvmCross/issues/267?source=c
     public class HackResourceLoaderPluginBootstrap
         : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.ResourceLoader.PluginLoader>
@@ -43,7 +33,6 @@ namespace Babel.Wpf
 
         protected override IMvxApplication CreateApp()
         {
-            Mvx.RegisterType<IMvxResourceLoader,HackMvxWpfResourceLoader>();
             return new Core.App();
         }
     }
