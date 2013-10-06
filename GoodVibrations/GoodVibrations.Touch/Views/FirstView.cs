6cbdf82ceeb40af300eca7a2787de79d501ec0e3
Stuart <lodge.stuart@gmail.com>
2013-10-06 14:32:04 +0100

    Good Vibrations up to current

--- GoodVibrations/GoodVibrations.Touch/Views/FirstView.cs
+++ GoodVibrations/GoodVibrations.Touch/Views/FirstView.cs
@@ -14,16 +14,18 @@ namespace GoodVibrations.Touch.Views
             View = new UIView(){ BackgroundColor = UIColor.White};
             base.ViewDidLoad();
 
-            var label = new UILabel(new RectangleF(10, 10, 300, 40));
-            Add(label);
-            var textField = new UITextField(new RectangleF(10, 50, 300, 40));
-            Add(textField);
-            var button = new UIButton(new RectangleF(10, 50, 300, 40));
+            //var label = new UILabel(new RectangleF(10, 10, 300, 40));
+            //Add(label);
+            //var textField = new UITextField(new RectangleF(10, 50, 300, 40));
+            //Add(textField);
+            var button = new UIButton(UIButtonType.RoundedRect);
+            button.Frame = new RectangleF(10, 50, 300, 40);
+            button.SetTitle("Shake", UIControlState.Normal);
             Add(button);
 
             var set = this.CreateBindingSet<FirstView, Core.ViewModels.FirstViewModel>();
-            set.Bind(label).To(vm => vm.Hello);
-            set.Bind(textField).To(vm => vm.Hello);
+            //set.Bind(label).To(vm => vm.Hello);
+            //set.Bind(textField).To(vm => vm.Hello);
             set.Bind(button).To(vm => vm.ShakeCommand);
             set.Apply();
         }
